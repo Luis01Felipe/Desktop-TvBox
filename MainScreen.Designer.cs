1960017f2a0846402ffb2c9c9f5768ec152ea723
Beater <104606257+Luis01Felipe@users.noreply.github.com>
2024-07-22 18:52:23 -0300

    Initial commit

---
+++ MainScreen.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Desktop_TvBox
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            this.btnYoutube = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbClock = new System.Windows.Forms.Label();
+            this.timer1 = new System.Timers.Timer();
+            this.lbDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnYoutube
+            // 
+            this.btnYoutube.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnYoutube.AutoSize = true;
+            this.btnYoutube.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnYoutube.BackgroundImage")));
+            this.btnYoutube.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnYoutube.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnYoutube.Location = new System.Drawing.Point(405, 145);
+            this.btnYoutube.Name = "btnYoutube";
+            this.btnYoutube.Size = new System.Drawing.Size(150, 250);
+            this.btnYoutube.TabIndex = 0;
+            this.btnYoutube.UseVisualStyleBackColor = true;
+            this.btnYoutube.Click += new System.EventHandler(this.btnYoutube_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(230, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 250);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(580, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 250);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lbClock
+            // 
+            this.lbClock.BackColor = System.Drawing.Color.Transparent;
+            this.lbClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClock.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbClock.Location = new System.Drawing.Point(373, 9);
+            this.lbClock.Name = "lbClock";
+            this.lbClock.Size = new System.Drawing.Size(200, 50);
+            this.lbClock.TabIndex = 3;
+            this.lbClock.Text = "00:00";
+            this.lbClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // lbDate
+            // 
+            this.lbDate.BackColor = System.Drawing.Color.Transparent;
+            this.lbDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbDate.Location = new System.Drawing.Point(373, 59);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(200, 50);
+            this.lbDate.TabIndex = 4;
+            this.lbDate.Text = "quarta, 11, jul";
+            this.lbDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.Controls.Add(this.lbDate);
+            this.Controls.Add(this.lbClock);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnYoutube);
+            this.Name = "MainScreen";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.MainScreen_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lbDate;
+
+        private System.Timers.Timer timer1;
+
+        private System.Windows.Forms.Label lbClock;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Button btnYoutube;
+
+        #endregion
+    }
+}
---
+++ MainScreen.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Desktop_TvBox
+{
+    public partial class MainScreen : Form
+    {
+        // Declare buttons as a class-level field
+        private List<Button> buttons;
+
+        public MainScreen()
+        {
+            InitializeComponent();
+        
+            // Initialize the buttons list with button instances
+            buttons = new List<Button> { button1, btnYoutube, button2 };
+
+            //FormBorderStyle = FormBorderStyle.None;
+            //WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnYoutube_Click(object sender, EventArgs e)
+        {
+            // Example operation: Print the names of all buttons in the list
+            foreach (var button in buttons)
+            {
+                Console.WriteLine(button.Name);
+            }
+
+            // If you want to find a specific button by name, ensure you have a valid name to search for
+            // This is just an illustrative example, as this.Name refers to the form's name, not the button's
+            var foundButton = buttons.FirstOrDefault(b => b.Name == "btnYoutube");
+            if (foundButton != null)
+            {
+                // Perform an action with foundButton
+                Console.WriteLine($"Found button: {foundButton.Name}");
+            }
+        }
+
+        private void ResizeComponents(Control control, int width, int height, int x, int y)
+        {
+            control.Size = new Size(width, height);
+            control.Location = new Point(x, y);
+        }
+        
+        private void MainScreen_Resize(object sender, EventArgs e)
+        {
+            // Buttons
+            int btnWidth = 300;
+            int btnHeight = 500;
+            int btnCenterX = (this.Width / 2) - (btnWidth / 2); 
+            int btnCenterY = (this.Height / 2) - (btnHeight / 2);
+            
+            ResizeComponents(btnYoutube, btnWidth, btnHeight, btnCenterX, btnCenterY); // Centralizado
+            ResizeComponents(button1, btnWidth, btnHeight, btnCenterX - (btnWidth+25), btnCenterY); // Esquerda do Centro
+            ResizeComponents(button2, btnWidth, btnHeight, btnCenterX + (btnWidth+25), btnCenterY); // Direita do Centro
+            
+            // Labels
+            ResizeComponents(lbClock, 200, 50, (this.Width/2)-(200/2), 0); // Top Center
+            ResizeComponents(lbDate, 200, 50, (this.Width/2)-(200/2), lbClock.Height); // Top Center-Down
+        }
+
+        private void ButtonMovement(Button clickedButton, List<Button> buttons)
+        {
+            
+        }
+        
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            lbClock.Text = DateTime.Now.ToString("HH:mm");
+            lbDate.Text = DateTime.Now.ToString("dddd, dd, MMM", new System.Globalization.CultureInfo("pt-BR")); // Tirar daqui depois
+        }
+    }
+}
